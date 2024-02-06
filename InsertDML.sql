a8f53c5262c250ab111de53c3427a69a34522ee5
ShivamRazdan <118751701+ShivamRazdan@users.noreply.github.com>
2024-02-06 10:11:03 +0530

    Update InsertDML.sql

--- InsertDML.sql
+++ InsertDML.sql
@@ -1,28 +1,28 @@
 INSERT INTO Patient(email,password,name,address,gender)
 VALUES
-('ramesh@gmail.com','hrishikesh13','Ramesh','Tamil Nadu', 'male'),
-('suresh@gmail.com','hrishikesh13','Suresh','Karnataka', 'male'),
-('rakesh@gmail.com','hrishikesh13','Rakesh','Gujarat', 'male')
+('ramesh@gmail.com','admin1234','Ramesh','Tamil Nadu', 'male'),
+('suresh@gmail.com','admin1234','Suresh','Karnataka', 'male'),
+('rakesh@gmail.com','admin1234','Rakesh','Gujarat', 'male')
 ;
 
 INSERT INTO MedicalHistory(id,date,conditions,surgeries,medication)
 VALUES
-(1,'19-01-14','Pain in abdomen','Heart Surgery','Crocin'),
-(2,'19-01-14','Frequent Indigestion','none','none'),
-(3,'19-01-14','Body Pain','none','Iodex')
+(1,'16-04-22','Pain in abdomen','Heart Surgery','Crocin'),
+(2,'05-05-22','Frequent Indigestion','none','none'),
+(3,'22-02-23','Body Pain','none','Iodex')
 ;
 
 INSERT INTO Doctor(email, gender, password, name)
 VALUES
-('hathalye7@gmail.com', 'male', 'hrishikesh13', 'Hrishikesh Athalye'),
-('hathalye8@gmail.com', 'male', 'hrishikesh13', 'Hrishikesh Athalye')
+('sachin18@gmail.com', 'male', 'admin1234', 'Sachin Narula'),
+('prabhu17@gmail.com', 'male', 'admin1234', 'Hemanth Prabhu')
 ;
 
 INSERT INTO Appointment(id,date,starttime,endtime,status)
 VALUES
-(1, '19-01-15', '09:00', '10:00', 'Done'),
-(2, '19-01-16', '10:00', '11:00', 'Done'),
-(3, '19-01-18', '14:00', '15:00', 'Done')
+(1, '19-07-23', '09:00', '10:00', 'Done'),
+(2, '19-07-23', '10:00', '11:00', 'Done'),
+(3, '19-07-23', '14:00', '15:00', 'Done')
 ;
 
 INSERT INTO PatientsAttendAppointments(patient,appt,concerns,symptoms)
@@ -51,20 +51,20 @@ VALUES
 
 INSERT INTO Diagnose(appt,doctor,diagnosis,prescription)
 VALUES
-(1,'hathalye7@gmail.com', 'Bloating', 'Ibuprofen as needed'),
-(2,'hathalye8@gmail.com', 'Muscle soreness', 'Stretch morning/night'),
-(3,'hathalye8@gmail.com', 'Vitamin Deficiency', 'Good Diet')
+(1,'sachin18@gmail.com', 'Bloating', 'Ibuprofen as needed'),
+(2,'prabhu17@gmail.com', 'Muscle soreness', 'Stretch morning/night'),
+(3,'prabhu17@gmail.com', 'Vitamin Deficiency', 'Good Diet')
 ;
 
 INSERT INTO DocsHaveSchedules(sched,doctor)
 VALUES
-(001,'hathalye7@gmail.com'),
-(002,'hathalye8@gmail.com')
+(001,'sachin18@gmail.com'),
+(002,'prabhu17@gmail.com')
 ;
 
 INSERT INTO DoctorViewsHistory(history,doctor)
 VALUES
-(1,'hathalye7@gmail.com'),
-(2,'hathalye8@gmail.com'),
-(3,'hathalye8@gmail.com')
+(1,'sachin18@gmail.com'),
+(2,'prabhu17@gmail.com'),
+(3,'prabhu17@gmail.com')
 ;
